3d8971900b3673cc543b5724ea747d334dd73f20
Yelloe-TranHuy <tranhuymadao123@gmail.com>
2024-09-24 15:17:12 +0700

    Update

---
+++ BaiKiemTra02/Data/Migrations/20240924080944_asp.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BaiKiemTra02.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class asp : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateCreate",
+                table: "LopHoc");
+
+            migrationBuilder.AddColumn<int>(
+                name: "NamNhapHoc",
+                table: "LopHoc",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "NamRaTruong",
+                table: "LopHoc",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SoLuongSinhVien",
+                table: "LopHoc",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TenLopHoc",
+                table: "LopHoc",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NamNhapHoc",
+                table: "LopHoc");
+
+            migrationBuilder.DropColumn(
+                name: "NamRaTruong",
+                table: "LopHoc");
+
+            migrationBuilder.DropColumn(
+                name: "SoLuongSinhVien",
+                table: "LopHoc");
+
+            migrationBuilder.DropColumn(
+                name: "TenLopHoc",
+                table: "LopHoc");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateCreate",
+                table: "LopHoc",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
